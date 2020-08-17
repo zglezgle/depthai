c22bac64c640936009394e5eb2425a20410dbdc9
alex-luxonis <alex@luxonis.com>
2020-08-17 23:23:24 +0300

    Update `-boot` help message, empty to skip boot

--- depthai_helpers/cli_utils.py
+++ depthai_helpers/cli_utils.py
@@ -168,7 +168,7 @@ def parse_args():
                         help="Used by board developers for debugging.")
     
     parser.add_argument("-boot", "--boot_image", default=None, type=str,
-                        help="USB-boot a specified firmware image file and exit.")
+                        help="USB-boot a specified firmware image file. Empty '' to skip boot.")
 
     parser.add_argument("-fusb2", "--force_usb2", default=None, action="store_true",
                         help="Force usb2 connection")
